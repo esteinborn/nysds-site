fed8a8aca3754cdcab1b8a8f8c61ac5a279c46be
Eric Steinborn <308007+esteinborn@users.noreply.github.com>
2025-06-17 12:30:31 -0400

    correrct the DEV url

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,6 +1,6 @@
 module.exports = {
     buildDate: new Date(),
-    url: process.env.ELEVENTY_ENV === 'development' ? 'http://localhost:8080' : 'https://designsystem.ny.gov',
+    url: process.env.ELEVENTY_ENV === 'development' ? 'http://localhost:8080' : 'https://esteinborn.github.io/nysds-site',
     title: process.env.ELEVENTY_ENV === 'development' ? '🔴 New York State Design System' : 'New York State Design System',
     description: "Guidelines, components, and tools for building digital services in New York State.",
     crawl: false,
